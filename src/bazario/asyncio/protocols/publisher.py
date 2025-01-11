eba18ec2c9f22071e33f807f8360d97f921fa9a7
Dark04072006 <darkgpt9898@gmail.com>
2025-01-12 01:32:16 +0300

    initial commit

--- src/bazario/asyncio/protocols/publisher.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol, runtime_checkable
-
-from bazario.markers import Notification
-
-
-@runtime_checkable
-class Publisher(Protocol):
-    async def publish(self, notification: Notification) -> None: ...
